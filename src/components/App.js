4bfae0c6cdcf192de0d4eb87bf9a34d870b59c2a
Create_first_repo <gauravchoundiye123@gmail.com>
2024-11-09 18:16:45 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const[count,setCount]=useState(0)
-  setInterval(()=>{
-    setCount(count+1)
-  },1000)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    // Cleanup function to clear the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
-        <p>You've been on this page for {count} seconds</p>
+      <p>You've been on this page for {count} seconds.</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
